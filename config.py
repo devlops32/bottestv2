e5e6cb68640cfb406065379a964984892cdcf5ef
devlopss <jensbens67@gmail.com>
2026-08-23 14:29:01 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py - Конфигурация бота
+
+BOT_TOKEN = "8658276956:AAFk_Vzsa-Xl50xMlgJyOLkHymv29xuv28w"
+ADMIN_IDS = [7317419505]  # Ваш Telegram ID
+
+PROXY_FILE = 'proxy.txt'
+PROXY_ROTATION = True
+
+AUTO_SELL_DAYS = 3
+AUTO_SELL_ENABLED = True
+CHECK_INTERVAL = 3600
+
+ITEMS_PER_PAGE = 8
+LIST_FILE = 'list.txt'
