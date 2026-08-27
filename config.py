bce6d0e2f79132830e3170a916d8d4f500fd5617
devlopss <jensbens67@gmail.com>
2026-08-27 14:27:33 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# config.py - Конфигурация бота
+
+BOT_TOKEN = "8658276956:AAFk_Vzsa-Xl50xMlgJyOLkHymv29xuv28w"
+ADMIN_IDS = [7317419505]
+
+PROXY_FILE = 'proxy.txt'
+PROXY_ROTATION = True
+
+AUTO_SELL_DAYS = 3
+AUTO_SELL_ENABLED = True
+CHECK_INTERVAL = 3600
+
+ITEMS_PER_PAGE = 8
+LIST_FILE = 'list.txt'
+
+# ============================================
+# НАСТРОЙКИ БЕКАПА
+# ============================================
+BACKUP_ENABLED = True
+BACKUP_INTERVAL = 3600
+BACKUP_MAX_FILES = 10
+BACKUP_DIR = 'backups'
